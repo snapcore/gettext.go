16ed59949986ed3eca2b67fefea53d6cec92655f
James Henstridge <james@jamesh.id.au>
2020-11-02 22:42:50 +0800

    mofile: use a memory mapped version of the catalog

--- test_utils.go
+++ test_utils.go
@@ -3,6 +3,7 @@ package gettext
 import "testing"
 
 func assert_equal(t *testing.T, expected string, got string) {
+	t.Helper()
 	if expected != got {
 		t.Logf("%s != %s", expected, got)
 		t.Fail()

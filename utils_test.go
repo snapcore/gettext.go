1793dafecf5453d38d512129a56928cebe8f7f1c
James Henstridge <james@jamesh.id.au>
2020-11-14 07:48:48 +0800

    locale: add gettext.UserLanguages function returning list of user's preferred languages

--- utils_test.go
+++ utils_test.go
@@ -1,11 +1,22 @@
 package gettext
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func assert_equal(t *testing.T, expected string, got string) {
 	t.Helper()
 	if expected != got {
 		t.Logf("%s != %s", expected, got)
+		t.Fail()
+	}
+}
+
+func assertDeepEqual(t *testing.T, expected, got interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, got) {
+		t.Logf("%v != %v", expected, got)
 		t.Fail()
 	}
 }

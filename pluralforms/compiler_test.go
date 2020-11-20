e0b83dfcfa8d705a99bdef38c828ea6a59f030c7
James Henstridge <james@jamesh.id.au>
2020-11-20 11:40:36 +0800

    pluralforms: run each example as a subtest

--- pluralforms/compiler_test.go
+++ pluralforms/compiler_test.go
@@ -6,40 +6,36 @@ import (
 	"testing"
 )
 
-type fixture struct {
-	PluralForm string
-	Fixture    []int
-}
-
 func TestCompiler(t *testing.T) {
 	f, err := os.Open("testdata/plural_forms.json")
 	if err != nil {
 		t.Fatal(err)
 	}
 	dec := json.NewDecoder(f)
-	var fixtures []fixture
+	var fixtures []struct {
+		PluralForm string
+		Fixture    []int
+	}
 	err = dec.Decode(&fixtures)
 	if err != nil {
 		t.Fatal(err)
 	}
 	for _, data := range fixtures {
-		expr, err := Compile(data.PluralForm)
-		if err != nil {
-			t.Errorf("'%s' triggered error: %s", data.PluralForm, err)
-		} else if expr == nil {
-			t.Logf("'%s' compiled to nil", data.PluralForm)
-			t.Fail()
-		} else {
+		data := data
+		t.Run(data.PluralForm, func(t *testing.T) {
+			expr, err := Compile(data.PluralForm)
+			if err != nil {
+				t.Fatal(err)
+			} else if expr == nil {
+				t.Fatalf("'%s' compiled to nil", data.PluralForm)
+			}
 			for n, e := range data.Fixture {
 				i := expr.Eval(uint32(n))
 				if i != e {
-					t.Logf("'%s' with n = %d, expected %d, got %d, compiled to %s", data.PluralForm, n, e, i, expr)
+					t.Logf("n = %d, expected %d, got %d, compiled to %s", n, e, i, expr)
 					t.Fail()
 				}
-				if i == -1 {
-					break
-				}
 			}
-		}
+		})
 	}
 }

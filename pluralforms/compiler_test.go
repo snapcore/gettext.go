122216d7d00e80cd0ec97bcf1c25018bc1ce49bb
James Henstridge <james@jamesh.id.au>
2020-11-20 13:14:17 +0800

    pluralforms: add some direct tests for the expression trees generated by the compiler.

--- pluralforms/compiler_test.go
+++ pluralforms/compiler_test.go
@@ -3,8 +3,17 @@ package pluralforms
 import (
 	"encoding/json"
 	"os"
+	"reflect"
 	"testing"
 )
+
+func assertEqual(t *testing.T, expected, got interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, got) {
+		t.Logf("%#v != %#v", expected, got)
+		t.Fail()
+	}
+}
 
 func TestCompiler(t *testing.T) {
 	f, err := os.Open("testdata/plural_forms.json")
@@ -37,5 +46,81 @@ func TestCompiler(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func TestParser(t *testing.T) {
+	expr, err := Compile("1+n/5*10")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertEqual(t, expr, addExpr{
+		left: numberExpr{1},
+		right: mulExpr{
+			left: divExpr{
+				left:  varExpr{},
+				right: numberExpr{5},
+			},
+			right: numberExpr{10},
+		},
+	})
+
+	expr, err = Compile("1-(2+n)/3")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertEqual(t, expr, subExpr{
+		left: numberExpr{1},
+		right: divExpr{
+			left: addExpr{
+				left:  numberExpr{2},
+				right: varExpr{},
+			},
+			right: numberExpr{3},
+		},
+	})
+
+	expr, err = Compile("(n==1)?0:n>=2&&n<=4?1:2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertEqual(t, expr, ternaryExpr{
+		test: eqExpr{
+			left:  varExpr{},
+			right: numberExpr{1},
+		},
+		ifTrue: numberExpr{0},
+		ifFalse: ternaryExpr{
+			test: andExpr{
+				left: gteExpr{
+					left:  varExpr{},
+					right: numberExpr{2},
+				},
+				right: lteExpr{
+					left:  varExpr{},
+					right: numberExpr{4},
+				},
+			},
+			ifTrue:  numberExpr{1},
+			ifFalse: numberExpr{2},
+		},
+	})
+}
+
+func TestParserFailures(t *testing.T) {
+	for _, expr := range []string{
+		"1 + + 2",
+		"n=1",
+		"(n==1",
+		"1 +",
+		"m==1",
+		"n=>1",
+		"n>1 ? 0",
+	} {
+		_, err := Compile(expr)
+		if err == nil {
+			t.Logf("Expression %q unexpectedly compiled", expr)
+			t.Fail()
+		}
 	}
 }
